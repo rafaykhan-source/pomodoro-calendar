e09663e60e5c4a85061e3d70fd13ae37ea0287a7
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-21 01:55:14 -0400

    added a typehint

--- pomocalcontainer/pomodorocalendar/views.py
+++ pomocalcontainer/pomodorocalendar/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse("Hello, world. You're at the pomodorocalendar index.")
