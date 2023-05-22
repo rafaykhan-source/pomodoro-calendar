1fa2cf14faed0022ff43bde67bde37fb36fe59e2
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-21 20:25:47 -0400

    switching to postgresql

--- pomocalcontainer/pomocal/settings.py
+++ pomocalcontainer/pomocal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "pomocal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pomocaldb",
+        "USER": "mydatabaseuser",
+        "PASSWORD": "mypassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
