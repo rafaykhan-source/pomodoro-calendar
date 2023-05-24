5271a597803a1e10ab145033b61749c4968b43fb
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-24 02:05:07 -0400

    ran black on pomo.py

--- pomo.py
+++ pomo.py
@@ -6,30 +6,41 @@ from dotenv import load_dotenv
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
 
+
 def configure():
     load_dotenv()
     return
 
-def ding(): 
+
+def ding():
     os.system("afplay " + "ding.mp3")
     return
+
 
 def initialize_calendar():
     configure()
     global gc
-    gc = GoogleCalendar(calendar=os.getenv('cal'), credentials_path='credentials.json')
+    gc = GoogleCalendar(calendar=os.getenv("cal"), credentials_path="credentials.json")
     return
 
+
 def log_event(name, start, end, desc):
-    block = Event(name, start=start, end=end, desc=desc, color_id=random.randrange(1, 12))
+    block = Event(
+        name, start=start, end=end, desc=desc, color_id=random.randrange(1, 12)
+    )
     gc.add_event(block)
     print("Interval successfully logged.")
     return
 
+
 def log_response_query(name, start, end, desc):
-    log_response = input("Would you like to log this interval on Google Calendar? (Yes or No)").lower()
-    if 'yes' in log_response: log_event(name, start, end, desc)
+    log_response = input(
+        "Would you like to log this interval on Google Calendar? (Yes or No)"
+    ).lower()
+    if "yes" in log_response:
+        log_event(name, start, end, desc)
     return
+
 
 def report_block(study_sequence, seconds):
     minutes = seconds // 60
@@ -37,19 +48,22 @@ def report_block(study_sequence, seconds):
     hours = minutes // 60
     minutes = minutes % 60
     study_sequence.append(f"{int(hours)}:{int(minutes)}:{seconds:.2f}")
-    
-    
-    
+
+
 def traditional():
-    pomodoro_time = float(input("How long, in minutes, would you like each study block to be? "))
+    pomodoro_time = float(
+        input("How long, in minutes, would you like each study block to be? ")
+    )
     rest_time = float(input("How long, in minutes, would you like your breaks to be? "))
 
     while True:
         desc = input("Please enter a description of your Study Block: ")
-        input(f"Press Enter/Return to start the your {pomodoro_time} minute study block.")
+        input(
+            f"Press Enter/Return to start the your {pomodoro_time} minute study block."
+        )
         start = dt.datetime.now()
         print(f"Your study block has begun! {start.strftime('%X')}")
-        time.sleep(pomodoro_time*60)
+        time.sleep(pomodoro_time * 60)
         ding()
         end = dt.datetime.now()
         print(f"Your study block has ended! {end.strftime('%X')}")
@@ -57,17 +71,19 @@ def traditional():
         input(f"Press Enter/Return to start your {rest_time} minute break.")
         start = dt.datetime.now()
         print(f"Your break has begun! {start.strftime('%X')}")
-        time.sleep(rest_time*60)
+        time.sleep(rest_time * 60)
         ding()
         end = dt.datetime.now()
         print(f"Your break has ended! {end.strftime('%X')}")
         log_response_query("Break", start, end, "Break")
-        if 'n' in input("Would you like to continue studying? (Yes or No)").lower(): break
+        if "n" in input("Would you like to continue studying? (Yes or No)").lower():
+            break
     return
+
 
 def personal():
     study_sequence = []
-    
+
     while True:
         desc = input("Please enter what you're doing: ")
         input("Press Enter/Return to start studying. ")
@@ -80,7 +96,8 @@ def personal():
         print(f"Your study block has ended! {end.strftime('%X')}")
         log_response_query("Pomodoro Session", start, end, desc)
         report_block(study_sequence, timer_end - timer_start)
-        if 'n' in input("Would you like to continue studying? (Yes or No)").lower(): break
+        if "n" in input("Would you like to continue studying? (Yes or No)").lower():
+            break
         input("Press Enter/Return to start a break.")
         timer_start = time.time()
         start = dt.datetime.now()
@@ -92,13 +109,17 @@ def personal():
         log_response_query("Break", start, end, "Break")
         report_block(study_sequence, timer_end - timer_start)
         print("Your study sequence thus far is:", study_sequence)
-        if 'n' in input("Would you like to continue studying? (Yes or No)").lower(): break
+        if "n" in input("Would you like to continue studying? (Yes or No)").lower():
+            break
     return
+
 
 # Program Start
 
-sequence_choice = input("""Would you like to develop a personalized study sequence, or follow a traditional study sequence? 
-Please respond either 'personal' or 'traditional': """).lower()
+sequence_choice = input(
+    """Would you like to develop a personalized study sequence, or follow a traditional study sequence? 
+Please respond either 'personal' or 'traditional': """
+).lower()
 
 initialize_calendar()
 
