9e8f5ea33d06c5248201be312466405b0ae68c20
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-24 02:57:50 -0400

    made preliminary models

--- pomocalcontainer/pomodorocalendar/models.py
+++ pomocalcontainer/pomodorocalendar/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Session(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.start_time
+
+
+class Task(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
