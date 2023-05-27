ff97a491273268747432a578f9589674cd136f8c
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-27 04:22:14 -0400

    updated settings.py for cors headers

--- pomocalcontainer/pomocal/settings.py
+++ pomocalcontainer/pomocal/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "rest_framework",
+    "corsheaders",
     "pomodorocalendar.apps.PomodorocalendarConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +55,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "pomocal.urls"
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
