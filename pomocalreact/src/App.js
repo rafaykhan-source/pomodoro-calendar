0fe71024dd3ef3909588facb4d57241fc81684ec
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-27 04:23:13 -0400

    separated consumption from components + bp

--- pomocalreact/src/App.js
+++ pomocalreact/src/App.js
@@ -1,31 +1,19 @@
 import "./App.css";
+import { getTasks, getNotes, getSessions } from "./consumer.js";
 
-function getTasks() {
-  // TODO: fetch tasks from API
-  // fetch('http://127.0.0.1:8000/tasks/')
-  // .then(data => {
-  //   console.log(data.json())
-  //   return data.json();
-  // })
-  return [{
-    id: 1,
-    name: "Add Restful API for Task",
-    description: "- Necessary for downstream consumption by react",
-    completed: false,
-  }];
-}
-
-const tasks = getTasks();
+const tasks = await getTasks();
+const notes = await getNotes();
+const sessions = await getSessions();
 
 function TaskList() {
-  const listItems = tasks.map(task =>
+  const listItems = sessions.map(task =>
     <li
       key={task.id}
       style={{
         color: task.completed ? 'magenta' : 'darkgreen'
       }}
     >
-      {task.name + ": " + task.description}
+      {task.name + ": " + task.type}
     </li>
   );
 
