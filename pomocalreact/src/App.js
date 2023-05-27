16383d0f9c8f10d14e06223646b70df9a7642bc3
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-27 02:09:05 -0400

    adding react app

---
+++ pomocalreact/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
