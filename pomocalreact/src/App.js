b7b85ed1ba4c1140f64306185c06bca708548d52
Rafay Khan <raf.d.khan10@gmail.com>
2023-05-27 02:27:19 -0400

    created TaskList React component

--- pomocalreact/src/App.js
+++ pomocalreact/src/App.js
@@ -1,18 +1,37 @@
-import './App.css';
+import "./App.css";
 
-function MyButton() {
+function getTasks() {
+  // TODO: fetch tasks from API
+  // fetch('http://127.0.0.1:8000/tasks/')
+  // .then(data => {
+  //   console.log(data.json())
+  //   return data.json();
+  // })
+  return [{
+    id: 1,
+    name: "Add Restful API for Task",
+    description: "- Necessary for downstream consumption by react",
+    completed: false,
+  }];
+}
+
+const tasks = getTasks();
+
+function TaskList() {
+  const listItems = tasks.map(task =>
+    <li
+      key={task.id}
+      style={{
+        color: task.completed ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {task.name + ": " + task.description}
+    </li>
+  );
+
   return (
-    <button>I'm a button</button>
+    <ul>{listItems}</ul>
   );
 }
 
-function App() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
-}
-
-export default App;
+export default TaskList;
